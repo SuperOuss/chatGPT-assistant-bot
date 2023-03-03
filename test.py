60e06e18e7d8cfb39c5cd8b5a39ed802d301f960
SuperOuss <50530856+SuperOuss@users.noreply.github.com>
2023-03-03 11:42:16 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,113 @@
+import requests
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+from collections import deque
+from html.parser import HTMLParser
+from urllib.parse import urlparse
+import os
+import pandas as pd
+import tiktoken
+import openai
+from openai.embeddings_utils import distances_from_embeddings
+import numpy as np
+from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
+import numpy as np
+from openai.embeddings_utils import distances_from_embeddings
+openai.api_key = "openAI key"
+df=pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+df.head()
+
+#Creat context
+
+def create_context(
+    question, df, max_len=1800, size="ada"
+):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
+
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+        
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+        
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+        
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return "\n\n###\n\n".join(returns)
+
+def answer_question(
+    df,
+    model="gpt-3.5-turbo",
+    question="How can I create a data source within the Fliplet platform? ",
+    max_len=1800,
+    size="ada",
+    debug=False,
+    max_tokens=150,
+    stop_sequence=None
+):
+    """
+    Answer a question based on the most similar context from the dataframe texts
+    """
+    context = create_context(
+        question,
+        df,
+        max_len=max_len,
+        size=size,
+    )
+    # If debug, print the raw model response
+    if debug:
+        print("Context:\n" + context)
+        print("\n\n")
+
+    try:
+        # Create a completions using the question and context
+        response = openai.ChatCompletion.create(
+            # prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+            temperature=0.5,
+            max_tokens=max_tokens,
+            messages=[
+                     {"role": "assistant", "content": f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:"}],
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=stop_sequence,
+            model=model,
+        )
+        ##print(response)
+        return response["choices"][0]["message"]["content"].strip()
+    except Exception as e:
+        print(e)
+        return ""
+
+
+################################################################################
+### Type your test questions
+################################################################################
+
+print(answer_question(df, question="Is there a way to make questions appear conditionally based on a user's response? I know there are conditional containers but am not sure if that will work for a conditional follow-up question."))
+
+print(answer_question(df, question="We are wondering if we could create a link on one of our pages that automatically dials a number?"))
+
+print(answer_question(df, question="Hi there, I am trying to upload my materials to the app and it keeps taking me back to the home page/event list. could you please asssit?"))
+
+print(answer_question(df, question="Hi there - I have added email responses when a form is submitted but they are not appearing in my inbox. They are being sent to the email that is in the BCC box though"))
